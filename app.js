250c46d71063c8d829c3bdd133020abbcb0d6f47
Victor Nunez <vnunez0914@gmail.com>
2019-09-20 13:28:55 -0500

    added recipe api

---
+++ app.js
@@ -0,0 +1,13 @@
+$("#search").on("click", function(){
+    var searchTerm = $("#search").val()//get id for search button 
+    var queryURL = "https://api.edamam.com/search?q="+ searchTerm +" &app_id=65ef0c30&app_key=33cd1b3b70a3582a44c9c59516afc54c&from=0&to=9&calories=591-722&health=alcohol-free";
+
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    })
+
+    .then(function(response){
+        console.log(response)
+    })
+})

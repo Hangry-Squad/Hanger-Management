ea953f4b653f7df34caa2db2171fe9bcb34f4180
Victor Nunez <vnunez0914@gmail.com>
2019-09-24 13:48:04 -0500

    created loop and if/else statement to display new content

--- app.js
+++ app.js
@@ -40,7 +40,7 @@ $("#button-secondary").on("click", function(event){
         for(var i = 0; i < results.length; i++){
 
             //create div
-            var recipeAnchor = $("<a>");
+            var recipeAnchor = $("<div>");
             console.log(i);
             
             // create tag to store recipe name (label)
@@ -53,11 +53,15 @@ $("#button-secondary").on("click", function(event){
             // recipeAnchor.attr("href", "nutPageTest.html")//change to final nutrition page name
             recipeImage.attr("src", results[i].recipe.image)
             recipeImage.attr("alt", results[i].recipe.label)
-        
+            recipeImage.attr("data-ingredient", results[i].recipe.ingredientLines)
+            recipeImage.attr("data-yield", "Yield " + results[i].recipe.yield)
+            recipeImage.attr("href", results[i].recipe.url)
+            
 
             //Append the h3 and image tags to recipeDiv
             recipeAnchor.append(h);
             recipeAnchor.append(recipeImage);
+          
 
             // get jquery selector to append recipeDiv
             $(".display-results").append(recipeAnchor);
@@ -67,6 +71,118 @@ $("#button-secondary").on("click", function(event){
     })
 })
 
+$(document).on("click", ".results", function(){
+    var imageHolder = $(this)
+    console.log(imageHolder)
+    // console.log(imageHolder[0].children[1].attributes[2].value)
+
+    //make a div to hold everything were looping through
+    var selectedRecipeDiv = $("<div>");
+    for (let i = 0; i < imageHolder[0].children[1].attributes.length; i++) {
+
+        console.log(imageHolder[0].children[1].attributes[i].value)
+        if( imageHolder[0].children[1].attributes[i].value===imageHolder[0].children[1].attributes[0].value){
+            console.log("in if statment")
+            var selectedRecipeImg = $("<img>")
+            selectedRecipeImg.attr("src", imageHolder[0].children[1].attributes[0].value)
+            selectedRecipeImg.css("width", "5%")
+            selectedRecipeDiv.append(selectedRecipeImg)
+
+        }else if( imageHolder[0].children[1].attributes[i].value === imageHolder[0].children[1].attributes[4].value){
+        console.log("inside a Tag")
+            var selectedRecipeATag = $("<a>");
+            selectedRecipeATag.attr("href", imageHolder[0].children[1].attributes[4].value)
+            selectedRecipeATag.text(imageHolder[0].children[1].attributes[4].value)
+            selectedRecipeDiv.append(selectedRecipeATag);
+        }else {
+            var selectedRecipePTag = $("<p>");
+            selectedRecipePTag.text(imageHolder[0].children[1].attributes[i].value)
+            selectedRecipeDiv.append(selectedRecipePTag)
+        }
+        
+    }
+
+    $(".display-results").empty();
+    $(".display-results").append(selectedRecipeDiv);
+    $(".display-results").show()
+
+
+    // $(".display-results").hide()
+})
+
+
+    //clears search bar.
 function clear() {
     $(".display-results").empty();// get final selector name
   }
+
+  //start of js code for nutrition page (third page)
+
+
+//This should be calling whatever button (picture they chose to see the recipe of (2nd page)) they clicked 
+//need to do on click this --> hide middle section --> need to figure out how to console log the recipe object
+$("#ingredient-submit").on("click", function () {
+    console.log("clicked")
+
+    // $(".input-group-recipe").hide();
+    // $(".input-group-ingredient").hide();
+
+
+    event.preventDefault();
+
+    //this foodItem will be calling the value of the p tag of ingrediants in the future
+    var foodItem = $("#ingredient-input").val();
+    //this code is replacing the spaces in the ingrediants list, so that 
+    foodItem.replace(' ', '%20')
+    //api call
+    var queryURL = "https://api.edamam.com/api/nutrition-data?app_id=54229f7b&app_key=4047020179cb06d777610301b960cc69&ingr=" + foodItem;
+    var pageIngrediants = $("#ingrediants").val();
+    console.log(pageIngrediants);
+
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(function (response) {
+        function inputSearch() {
+            console.log(foodItem);
+        }
+        inputSearch();
+
+        console.log(response);
+        var listNutrients = response.totalNutrients;
+        console.log(listNutrients);
+
+        var calorieContent = response.calories;
+        console.log("calories: " + calorieContent);
+        var calorieDisplay = $("<p>").text("calories: " + calorieContent);
+        $(".middle-content-section").prepend(calorieDisplay);
+
+
+        for (let nutrient in listNutrients) {
+
+            var nutrientLabel = listNutrients[nutrient].label;
+            var nutrientQuan = listNutrients[nutrient].quantity;
+            var nutrientUnit = listNutrients[nutrient].unit;
+            console.log(nutrientLabel);
+            console.log(nutrientQuan);
+            console.log(nutrientUnit);
+
+            var p = $("<p>").text(`${nutrientLabel} ${nutrientQuan} ${nutrientUnit}`);
+
+            $(".middle-content-section").append(p);
+
+        }
+    })
+})
+
+
+
+
+
+
+
+
+
+
+
+
